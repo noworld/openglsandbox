bfa4ea901f0dd69167075825dc94849c7a18b5ec
noworld <noworld@gmail.com>
2014-07-07 13:49:55 -0400

    Changing to use supplied coordinates.

--- SimpleRender/src/com/solesurvivor/simplerender/Font.java
+++ SimpleRender/src/com/solesurvivor/simplerender/Font.java
@@ -2,6 +2,7 @@ package com.solesurvivor.simplerender;
 
 import java.nio.FloatBuffer;
 
+import android.graphics.Point;
 import android.opengl.Matrix;
 
 
@@ -31,6 +32,22 @@ public class Font {
 	public int mNrmSize = 3;
 	public int mTxcSize = 2;
 	
+	
+	/* New - Draw fonts based on supplied measurements */
+	
+	public float mAtlasSzX = 1024.0f; //Font atlas size
+	public float mAtlasSzY = 1024.0f;
+	
+//	public float mBBTop = 767.0f;    //Top Y of first glyph bounding box
+//	public float mBBBottom = 702.0f; //Bottom Y of first glyph bounding box
+//	public float mBBLeft = 2.0f;   //Left X of first glyph bounding box
+//	public float mBBRight = 43.0f;  //Right X of first glyph bounding box
+	
+	public float mBBTop = 767.0f;    //Top Y of first glyph bounding box
+	public float mBBBottom = 702.0f; //Bottom Y of first glyph bounding box
+	public float mBBLeft = 2.0f;   //Left X of first glyph bounding box
+	public float mBBRight = 43.0f;  //Right X of first glyph bounding box
+	
 	public Font() {
 		Matrix.setIdentityM(mModelMatrix, 0);
 		loadFont();
@@ -42,6 +59,11 @@ public class Font {
 	
 	private void loadFont() {
 		
+		float mTop = 1 - (mBBTop / mAtlasSzY);  //Flip Y
+		float mBottom = 1 - (mBBBottom / mAtlasSzY); //Flip Y
+		float mLeft = mBBLeft / mAtlasSzX;
+		float mRight = mBBRight / mAtlasSzX;
+		
 		mIdx[0] = 0;
 		
 		mPosNrm[0] = -2.5f;
@@ -52,8 +74,11 @@ public class Font {
 		mPosNrm[4] = 0.0f;
 		mPosNrm[5] = 1.0f;
 
-		mTxc[0] = 0.00188744f;
-		mTxc[1] = 0.31482393f;
+//		Bottom Left Coordinate
+//		mTxc[0] = 0.01171875f;
+//		mTxc[1] = 0.31482393f;
+		mTxc[0] = mLeft;
+		mTxc[1] = mBottom;
 		
 		mIdx[1] = 1;
 		
@@ -65,9 +90,12 @@ public class Font {
 		mPosNrm[10] = 0.0f;
 		mPosNrm[11] = 1.0f;
 
-		mTxc[2] = 0.04236412f;
-		mTxc[3] = 0.3148241f;
-
+//		Bottom Right
+//		mTxc[2] = 0.04236412f;
+//		mTxc[3] = 0.3148241f;
+		mTxc[2] = mRight;
+		mTxc[3] = mBottom;
+		
 		mIdx[2] = 2;
 		
 		mPosNrm[12] = 2.5f;
@@ -78,8 +106,11 @@ public class Font {
 		mPosNrm[16] = 0.0f;
 		mPosNrm[17] = 1.0f;
 
-		mTxc[4] = 0.04236418f;
-		mTxc[5] = 0.2507475f;
+//		Top Right
+//		mTxc[4] = 0.04236418f;
+//		mTxc[5] = 0.2507475f;
+		mTxc[4] = mRight;
+		mTxc[5] = mTop;
 
 		mIdx[3] = 3;
 		
@@ -91,9 +122,11 @@ public class Font {
 		mPosNrm[22] = 0.0f;
 		mPosNrm[23] = 1.0f;
 
-		
-		mTxc[6] = 0.001887559f;
-		mTxc[7] = 0.25074738f;
+//		Top Left
+//		mTxc[6] = 0.001887559f;
+//		mTxc[7] = 0.25074738f;
+		mTxc[6] = mLeft;
+		mTxc[7] = mTop;
 
 		mIdx[4] = 4;
 		
@@ -105,8 +138,11 @@ public class Font {
 		mPosNrm[28] = 0.0f;
 		mPosNrm[29] = 1.0f;
 
-		mTxc[8] = 0.00188744f;
-		mTxc[9] = 0.31482393f;
+//		Bottom Left
+//		mTxc[8] = 0.00188744f;
+//		mTxc[9] = 0.31482393f;
+		mTxc[8] = mLeft;
+		mTxc[9] = mBottom;
 
 		mIdx[5] = 5;
 		
@@ -118,8 +154,11 @@ public class Font {
 		mPosNrm[34] = 0.0f;
 		mPosNrm[35] = 1.0f;
 
-		mTxc[10] = 0.04236418f;
-		mTxc[11] = 0.2507475f;
+//		Top Right
+//		mTxc[10] = 0.04236418f;
+//		mTxc[11] = 0.2507475f;
+		mTxc[10] = mRight;
+		mTxc[11] = mTop;
 		
 	}
 	
