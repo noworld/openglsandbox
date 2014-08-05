29347ecb23c913bce8c91f5d30838b24e9ef55cb
noworld <noworld@gmail.com>
2014-08-05 10:58:09 -0400

    Initial Commit

---
+++ HazeGrayData/src/com/solesurvivor/hazegray/DataClassEnum.java
@@ -0,0 +1,19 @@
+package com.solesurvivor.hazegray;
+
+public enum DataClassEnum {
+
+	SHIP,
+    AIRCRAFT,
+    GUN_SYSTEM,
+    MISSILE_SYSTEM,
+    ASW_SYSTEM,
+    TORPEDO,
+    AIRCRAFT_ORDNANCE,
+    AIRCRAFT_ELECTRONICS,
+    AIRCRAFT_GUN,
+    NUCLEAR_WEAPONS,
+    EARCH_RADAR,
+    WEAPON_DIRECTOR,
+    SONAR,
+    ENVIRONMENT
+}
