bf84a7d9daffa7c3eaf5c12d78c00de929fd1c7a
noworld <noworld@gmail.com>
2014-08-28 16:36:35 -0400

    It's a start...

--- SimpleRender2_5/res/raw/f_heightmap.glsl
+++ SimpleRender2_5/res/raw/f_heightmap.glsl
@@ -10,8 +10,18 @@ varying vec2 v_TexCoordinate;
   
 void main()                    		
 {                              
-    vec3 color = texture2D(u_Texture, v_TexCoordinate).rgb;
-    vec3 height = vec3(u_Height);
-    gl_FragColor = vec4((color + height), 1.0);               		
+
+	vec3 linev = vec3(u_LineVec, 0.0);
+	vec3 diff = v_Position - linev;
+	float dir = dot(diff, linev);
+	vec4 color = texture2D(u_Texture, v_TexCoordinate);
+	
+	if(dir > 0.0) {
+		vec3 height = vec3(u_Height);
+    	gl_FragColor = vec4((color.rgb + height), 1.0);	
+	} else {
+    	gl_FragColor = color;
+	}
+               		
 }                                                                     	
 
